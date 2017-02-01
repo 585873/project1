771c950e10280b8d2c73002b5b641863fa289733
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-01-31 23:12:15 -0500

    updtaed js

--- project1.js
+++ project1.js
@@ -9,3 +9,45 @@ for (var i=0;i < alphabet.length; i++) {
   $('.buttons').append('<button>'+alphabet[i]+'</button>');
 
 }
+
+ function reset() {
+  lettersClicked = "";
+}
+
+$("button.letter1").on("click", function(){
+  console.log("Geeeeeez");
+})
+function to add letters(clicked) to input field text box
+function game (g) {
+  location.reload();
+
+  var letterClicked = "";
+  var letterGuessed = "";
+
+
+if (letterClicked.indexof(g) != -1) {
+  console.log(g);
+    return;
+  }
+
+
+
+$("button .#letter1").on("click", function(){
+  console.log("Geeeeeez");
+})
+
+lettersClicked += g;
+$("button .letter1").on("click", function(){
+  // console.log("letterGuessed");
+  // // $("#button").pop(letterGuessed)
+  // letterClicked.push('dom')
+  // $(".letters").append("dom");
+  console.log("letterGuessed");
+
+})
+  // buttons();
+  play();
+  $("button.letterA").on("click", function(){
+    $(".letterA").hide();
+    console.log("You clicked Letter A");
+  })
