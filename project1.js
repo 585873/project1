9b4a86245b8de1104457307605795d1d2b83db2f
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-02-05 23:51:54 -0500

    updated js

--- project1.js
+++ project1.js
@@ -1,53 +1,133 @@
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-alphabet = alpha.split('');
-var guess;
-var hang;
-count = 4;
-imgCount = 1;
+//This is a function that creates aplphabet for user to select options
+window.onload = function () {
 
-for (var i=0;i < alphabet.length; i++) {
-  $('.buttons').append('<button>'+alphabet[i]+'</button>');
+  var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+        'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+  var chosenCategory;
+  var word ;
+  var guess ;
+  var geusses = [ ];
+  var lives ;
+
+  // Get elements returns the elements
+  var showLives = document.getElementById("mylives");
+  var showCatagory = document.getElementById("scatagory");
+
+  // create alphabet ul using for loop
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+
+	// Create geusses ul
+   result = function () {
+    wordHolder = document.getElementById('hold');
+    correct = document.createElement('ul');
+
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (word[i] === "-") {
+        guess.innerHTML = "-";
+        space = 1;
+      } else {
+        guess.innerHTML = "_";
+      }
+
+      geusses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
+  }
+
+  // this function shows remaining lives
+   comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      showLives.innerHTML = "Game Over buddy!";
+    }
+    for (var i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        showLives.innerHTML = "You Win and lets play again!";
+      }
+    }
+  }
+
+      // Animate man standing
+  var animate = function () {
+    var drawMe = lives ;
+    drawArray[drawMe]();
+  }
+
+  // OnClick Function creats
+   check = function () {
+    list.onclick = function () {
+      var geuss = (this.innerHTML);
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === geuss) {
+          geusses[i].innerHTML = geuss;
+          counter += 1;
+        }
+      }
+      var j = (word.indexOf(geuss));
+      if (j === -1) {
+        lives -= 1;
+        comments();
+        animate();
+      } else {
+        comments();
+      }
+    }
+  }
+
+
+  // Play function
+  play = function () {
+    categories = [
+        ["Football", "BASEBALL", "TENNIS", "CRICKET", "HOCKEY"],
+    ];
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    geusses = [ ];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+    selectCat();
+    canvas();
+  }
+
+  play();
+
+
+   // Reset
+
+  document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    context.clearRect(0, 0, 400, 400);
+    play();
+  }
 }
-// 
-//  function reset() {
-//   lettersClicked = "";
-// }
-//
-// $("button.letter1").on("click", function(){
-//   console.log("Geeeeeez");
-// })
-// function to add letters(clicked) to input field text box
-// function game (g) {
-//   location.reload();
-//
-//   var letterClicked = "";
-//   var letterGuessed = "";
-//
-//
-// if (letterClicked.indexof(g) != -1) {
-//   console.log(g);
-//     return;
-//   }
-//
-//
-//
-// $("button .#letter1").on("click", function(){
-//   console.log("Geeeeeez");
-// })
-//
-// lettersClicked += g;
-// $("button .letter1").on("click", function(){
-//   // console.log("letterGuessed");
-//   // // $("#button").pop(letterGuessed)
-//   // letterClicked.push('dom')
-//   // $(".letters").append("dom");
-//   console.log("letterGuessed");
-//
-// })
-//   // buttons();
-//   // play();
-//   // $("button.letterA").on("click", function(){
-//   //   $(".letterA");
-//   //   console.log("You clicked Letter A");
-//   // })
